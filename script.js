0027a35f57afff61966deac92c10d2222eca9a45
kleoncyrusbumanlag <kleon.bumanlag@edu.immaginazioneelavoro.it>
2025-11-24 16:55:02 +0100

    efef

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+let ore = 999;
+
 function assenze() {
     n = parseInt(document.getElementById("progresso").value);
     console.log(n);
-    ore = 999;
-    assenze = n * 0;
-    ore_assenti = ore - assenze;
-    percentuale = (100 * ore_assenti)/ore;
+    let assenze = n * 6;
+    let ore_assenti = ore - assenze;
+    let percentuale = (100 * ore_assenti)/ore;
     console.log(percentuale);
     document.getElementById("barra").style.width = percentuale + "%";
 }
@@ -13,6 +14,30 @@ function calcola() {
     let n1 = parseInt(document.getElementById("num1").value);
     let n2 = parseInt(document.getElementById("num2").value);
     let op = document.getElementById("operazione").value;
+
     let res;
-    if (isNaN(n1)) isNaN
+
+    if (isNaN(n1) || isNaN(n2)) {
+        document.getElementById("risultato").innerHTML = "Errore";
+        return;
+    }
+
+    if (op == "+") {
+        res = n1 + n2;
+    }
+    if (op == "-") {
+        res = n1 - n2;
+    }
+    if (op == "*") {
+        res = n1 * n2;
+    }
+    if (op == "/") {
+        if (n2 == 0) {
+            document.getElementById("risultato").innerHTML = "Divisione per 0 impossibile";
+            return;
+        }
+        res = n1 / n2;
+    }
+
+    document.getElementById("risultato").innerHTML = res;
 }
